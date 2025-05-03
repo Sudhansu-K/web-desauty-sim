a754ca8e40604b2895024c1bfca6b9d148cf91f1
sudhansuk <sudhansukujur2@gmail.com>
2025-05-03 11:41:43 +0530

    on off switch bug fix

--- ckt.py
+++ ckt.py
@@ -10,9 +10,7 @@ import numpy as np
 import pandas as pd
 
 from flask_socketio import SocketIO
-from flask import Flask, render_template,request,Response
-cxx=0;rr1=0;rr2=0;rr3=0;rr4=0;rr5=0;rr6=0;cd=0;
-
+from flask import Flask, render_template
 app=Flask(__name__)
 socket=SocketIO(app)
 
@@ -32,7 +30,7 @@ def cmessage(cm):
     rr4=float(cm['r5'])
     rr5=float(cm['r6'])
     rr6=float(cm['r7'])
-    print(type(cxx))
+    #print(rr1)
     #circuit logic
     cx=float(cxx)
     r4=float(rr5*2000+rr4*1000+rr3*100+rr2*10+rr1*1)
@@ -45,7 +43,7 @@ def cmessage(cm):
     ckt.V(5,'n2','n3',0@u_V)
     ckt.R(3,'n2',ckt.gnd,r3@u_kOhm) #variable resistor r3
     ckt.R(4,'n3',ckt.gnd,r4@u_kOhm) #variable resistor r4
-    print(ckt)
+    #print(ckt)
     sim=ckt.simulator(temperature=25,nominal_temperature=25)
     sim.initial_condition(n1=0,n2=0,n3=0)
     analysis=sim.transient(step_time=1@u_us,end_time=0.1@u_ms)
